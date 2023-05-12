87499c05b6f7216d2074c3db2fd10f37b5d9bc13
Brian <b.perez3237@gmail.com>
2023-05-12 12:23:34 -0400

    change render install

--- install_render.sh
+++ install_render.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Download and install the Render CLI
-curl -o render.tgz https://render.com/download/linux && tar -xvf render.tgz && ./render/install.sh
+curl https://render.com/download/linux --output render.tgz
+tar -xzf render.tgz
+./render/install.sh
 
 # Verify that the Render CLI is installed
 render --version
