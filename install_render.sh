1cca6b0677fea6edc66ee2290c5e2172db3f77af
Brian <b.perez3237@gmail.com>
2023-05-12 12:27:04 -0400

    change render install

--- install_render.sh
+++ install_render.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Download and install the Render CLI
-curl https://render.com/download/linux --output render.tgz
-tar -xzf render.tgz
-./render/install.sh
+# Install the Render CLI using Homebrew
+brew tap render-oss/render
+brew install render
 
 # Verify that the Render CLI is installed
 render --version
