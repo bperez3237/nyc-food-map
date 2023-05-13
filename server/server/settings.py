34221f2b14943a86c5e88701f73b9f5067934a49
Brian <b.perez3237@gmail.com>
2023-05-13 14:14:28 -0400

    Update django database settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -119,8 +119,12 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "NAME": "postgres",
         "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
