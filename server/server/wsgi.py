57bc49dcd9bfd89412509b37308ccabc2e34f7bb
brian <b.perez3237@gmail.com>
2023-05-16 14:16:02 -0400

    Update wsgi

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.server.settings")
+# Get the directory containing the wsgi.py file
+wsgi_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Add the directory containing the Django project to the system path
+sys.path.append(wsgi_dir)
+
+# Set the Django settings module based on the wsgi.py location
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
+# Create the WSGI application
 application = get_wsgi_application()
