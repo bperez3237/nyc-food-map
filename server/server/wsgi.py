e98ded898b3ad12d7b2086d3e83d3df22404c092
brian <b.perez3237@gmail.com>
2023-05-16 16:15:48 -0400

    Edit wsgi for render deploy

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -13,10 +13,10 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 # Get the directory containing the wsgi.py file
-wsgi_dir = os.path.dirname(os.path.abspath(__file__))
+# wsgi_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Add the directory containing the Django project to the system path
-sys.path.append(wsgi_dir)
+# sys.path.append(wsgi_dir)
 
 # Set the Django settings module based on the wsgi.py location
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.server.settings")
