30bad889f9c8c4c05bb038d298c68205741a82a5
brian <b.perez3237@gmail.com>
2023-05-17 15:11:00 -0400

    update render build comamnds

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # builds the front end code
 rm -rf public
 npm install --prefix jwt-react
-CI=false npm run build --prefix jwt-react
+npm run build --prefix jwt-react
 cp -a jwt-react/build/. public/
 
 # builds the back end code
